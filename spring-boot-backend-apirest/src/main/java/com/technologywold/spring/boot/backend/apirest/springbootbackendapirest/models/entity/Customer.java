b79dc18dcb7f60631e67f7d1bebba450fc6235cc
geofisica89 <davidosirisayala@gmail.com>
2023-03-06 00:23:15 -0300

    init project backend springboot

---
+++ spring-boot-backend-apirest/src/main/java/com/technologywold/spring/boot/backend/apirest/springbootbackendapirest/models/entity/Customer.java
@@ -0,0 +1,27 @@
+package com.technologywold.spring.boot.backend.apirest.springbootbackendapirest.models.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+@Entity
+@Table(name = "customer")
+public class Customer implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    private String name;
+    @Column(name = "last_name")
+    private String lastName;
+    private String email;
+    @Column(name = "created_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+    private static final long serialVersionUID = 1L;
+}
