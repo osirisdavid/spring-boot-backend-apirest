0415fd29e321b805a2610c1149a02435f65ad8bd
geofisica89 <davidosirisayala@gmail.com>
2023-03-07 20:35:37 -0300

    configuration crossOrigin

--- spring-boot-backend-apirest/src/main/java/com/technologywold/spring/boot/backend/apirest/springbootbackendapirest/controller/CustomerRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/technologywold/spring/boot/backend/apirest/springbootbackendapirest/controller/CustomerRestController.java
@@ -3,12 +3,11 @@ package com.technologywold.spring.boot.backend.apirest.springbootbackendapirest.
 import com.technologywold.spring.boot.backend.apirest.springbootbackendapirest.models.entity.Customer;
 import com.technologywold.spring.boot.backend.apirest.springbootbackendapirest.models.service.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class CustomerRestController {
