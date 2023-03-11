5b3584634a1b610905855e43981ace5130ecdf3e
geofisica89 <davidosirisayala@gmail.com>
2023-03-10 23:28:03 -0300

    manejo de errores class controller

--- spring-boot-backend-apirest/src/main/java/com/technologywold/spring/boot/backend/apirest/springbootbackendapirest/controller/CustomerRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/technologywold/spring/boot/backend/apirest/springbootbackendapirest/controller/CustomerRestController.java
@@ -3,10 +3,15 @@ package com.technologywold.spring.boot.backend.apirest.springbootbackendapirest.
 import com.technologywold.spring.boot.backend.apirest.springbootbackendapirest.models.entity.Customer;
 import com.technologywold.spring.boot.backend.apirest.springbootbackendapirest.models.service.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -24,8 +29,21 @@ public class CustomerRestController {
 
     @GetMapping("/customer")
     @ResponseStatus(HttpStatus.OK)
-    public Customer show(@PathVariable Long id){
-        return iCustomerService.finById(id);
+    public ResponseEntity<?> show(@PathVariable Long id){
+        Customer customer = null;
+        Map<String, Object> response = new HashMap<>();
+        try{
+            customer = iCustomerService.finById(id);
+        } catch (DataAccessException e){
+            response.put("mesage", "Error al realizar consulta en la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        if(customer == null){
+            response.put("mesage", "El cliente ID: ".concat(id.toString().concat(" no existe")));
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Customer>(customer, HttpStatus.OK);
     }
 
     @PostMapping("/customer")
