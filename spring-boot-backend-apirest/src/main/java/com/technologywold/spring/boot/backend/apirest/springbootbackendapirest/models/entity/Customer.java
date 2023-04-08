62a109af03564a0b75aa2e2c3e6ffaa10e2c3b91
osirisdavid <davidosirisayala@gmail.com>
2023-04-08 18:41:47 -0300

    validaciones con anotaciones en la clase entity

--- spring-boot-backend-apirest/src/main/java/com/technologywold/spring/boot/backend/apirest/springbootbackendapirest/models/entity/Customer.java
+++ spring-boot-backend-apirest/src/main/java/com/technologywold/spring/boot/backend/apirest/springbootbackendapirest/models/entity/Customer.java
@@ -3,6 +3,9 @@ package com.technologywold.spring.boot.backend.apirest.springbootbackendapirest.
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -12,22 +15,33 @@ import java.util.Date;
 @Entity
 @Table(name = "customer")
 public class Customer implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @NotEmpty
+    @Size(min=4, max=12)
     @Column(nullable = false)
     private String name;
+
+    @NotEmpty
+    @Email
     @Column(name = "last_name")
     private String lastName;
+
     @Column(nullable = false, unique = true)
     private String email;
+
     @Column(name = "created_at")
     @Temporal(TemporalType.DATE)
     private Date createAt;
+
     @PrePersist
     public void prePersist(){
         createAt = new Date();
     }
+
     private static final long serialVersionUID = 1L;
 }
